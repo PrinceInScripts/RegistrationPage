a3985ae4f84256e65f8166b1abd442766cee72e6
Prince Kumar <princepk7878kumar@gmail.com>
2023-12-06 13:47:28 +0530

    basic funionality make

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express=require('express')
+const authRouter = require('./routers/auth.routers')
 
 const app=express()
+
+app.use(express.json())
+
+app.use('/api/auth',authRouter)
 
 app.use('/',(req,res)=>{
     res.status(200).json({
