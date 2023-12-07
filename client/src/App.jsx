dadb5bf511bf5adc900072a808d9a70a7233c8dd
Prince Kumar <princepk7878kumar@gmail.com>
2023-12-07 14:11:33 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router"
+import Home from "./pages/Home"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
 
 function App() {
 
   return (
     <>
-     <h1 className="bg-black text-white">hii</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="signup" element={<Signup/>}/>
+        <Route path="signin" element={<Signin/>}/>
+
+      </Routes>
     </>
   )
 }
